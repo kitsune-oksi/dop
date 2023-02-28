02a9c5e6b1a22cf4685597eeef0fca24cfcf3c88
OksanaN <nurova.oksana2020@gmail.com>
2023-02-28 11:18:16 +0500

    stading react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
-import {v1} from 'uuid';
+import {Site} from "./components/Site";
 
-export type FilterValuesType = "all" | "active" | "completed";
-
-type todolistsType = {
-    id: string
-    title: string
-    filter: FilterValuesType
-}
 
 function App() {
-
-    // let [tasks, setTasks] = useState([
-    //     {id: v1(), title: "HTML&CSS", isDone: true},
-    //     {id: v1(), title: "JS", isDone: true},
-    //     {id: v1(), title: "ReactJS", isDone: false},
-    //     {id: v1(), title: "Rest API", isDone: false},
-    //     {id: v1(), title: "GraphQL", isDone: false},
-    // ]);
-    // let [filter, setFilter] = useState<FilterValuesType>("all");
-
-    let todolistID1 = v1();
-    let todolistID2 = v1();
-
-    let [todolists, setTodolists] = useState<Array<todolistsType>>([
-        {id: todolistID1, title: 'What to learn', filter: 'all'},
-        {id: todolistID2, title: 'What to buy', filter: 'all'},
-    ])
-
-    let [tasks, setTasks] = useState({
-        [todolistID1]: [
-            {id: v1(), title: "HTML&CSS", isDone: true},
-            {id: v1(), title: "JS", isDone: true},
-            {id: v1(), title: "ReactJS", isDone: false},
-            {id: v1(), title: "Rest API", isDone: false},
-            {id: v1(), title: "GraphQL", isDone: false},
-        ],
-        [todolistID2]: [
-            {id: v1(), title: "HTML&CSS2", isDone: true},
-            {id: v1(), title: "JS2", isDone: true},
-            {id: v1(), title: "ReactJS2", isDone: false},
-            {id: v1(), title: "Rest API2", isDone: false},
-            {id: v1(), title: "GraphQL2", isDone: false},
-        ]
-    });
-
-    function removeTask(taskId: string, todolistId: string) {
-        let filteredTasks = tasks[todolistId].filter(t => t.id != taskId);
-        setTasks({...tasks, [todolistId]: filteredTasks});
-    }
-
-    function addTask(title: string, todolistId: string) {
-        let task = {id: v1(), title: title, isDone: false};
-        let newTasks = [task, ...tasks[todolistId]];
-        setTasks({...tasks, [todolistId]: newTasks});
-    }
-
-    function changeStatus(taskId: string, isDone: boolean, todolistId: string) {
-        let task = tasks[todolistId].find(t => t.id === taskId);
-        if (task) {
-            task.isDone = isDone;
-        }
-
-        setTasks({...tasks, [todolistId]: tasks[todolistId].map(t => t.id === taskId ? {...t, isDone} : t)});
-    }
-
-
-    function changeFilter(value: FilterValuesType, todolistId: string) {
-        setTodolists(todolists.map(t => t.id === todolistId ? {...t, filter: value} : t))
-    }
-
-    function removeTodolist(todolistId: string) {
-        setTodolists(todolists.filter(t => t.id !== todolistId))
-        delete tasks[todolistId]
-    }
-
-
     return (
-        <div className="App">
-            {todolists.map(t => {
-
-                    let tasksForTodolist = tasks[t.id];
-
-                    if (t.filter === "active") {
-                        tasksForTodolist = tasksForTodolist.filter(t => !t.isDone);
-                    }
-                    if (t.filter === "completed") {
-                        tasksForTodolist = tasksForTodolist.filter(t => t.isDone);
-                    }
-
-                    return <Todolist key={t.id}
-                                     id={t.id}
-                                     title={t.title}
-                                     tasks={tasksForTodolist}
-                                     removeTask={removeTask}
-                                     changeFilter={changeFilter}
-                                     addTask={addTask}
-                                     changeTaskStatus={changeStatus}
-                                     filter={t.filter}
-                                     removeTodolist={removeTodolist}
-                    />
-                }
-            )}
+        <div>
+            <Site/>
         </div>
     );
 }
+
 
 export default App;
---
+++ src/components/pages/Page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {DataStateType} from "../../data/dataState";
+import {useParams} from "react-router-dom";
+import {Content} from "../Content";
+
+export const Page = (props: DataStateType) => {
+    const params = useParams()
+    return (
+        <div>
+            <Content page={props.pages[Number(params.id)]}/>
+        </div>
+    );
+};
+
